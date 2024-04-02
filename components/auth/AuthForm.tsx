5c07cf09c908b329f453f54316d173674324849d
WiseyXD <aryan.s.nag@gmail.com>
2024-04-02 08:12:57 +0530

    build : checking git

--- components/auth/AuthForm.tsx
+++ components/auth/AuthForm.tsx
@@ -63,6 +63,7 @@ export default function AuthForm({
             title: 'Values Submitted',
         });
     }
+    // git fuck up
 
     return (
         // <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
