626dc8eeec89fe81fd36675ddbcdcc0df8344d25
WiseyXD <aryan.s.nag@gmail.com>
2024-04-04 20:46:03 +0530

    build : added user role

--- auth.ts
+++ auth.ts
@@ -1,7 +1,16 @@
 import authConfig from '@/auth.config';
 import prisma from '@/lib/db';
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import { UserRole } from '@prisma/client';
 import NextAuth from 'next-auth';
+import { getUserById } from './data/user';
+
+declare module 'next-auth' {
+    interface User {
+        /** The user's postal address. */
+        role: 'ADMIN' | 'USER';
+    }
+}
 
 export const {
     handlers: { GET, POST },
@@ -10,14 +19,26 @@ export const {
     signOut,
 } = NextAuth({
     callbacks: {
-        async jwt({ token, account }) {
+        async jwt({ token, account, profile }) {
             // Persist the OAuth access_token to the token right after signin
+
+            if (!token.sub) return token;
+
+            const existingUser = await getUserById(token.sub);
+
+            if (!existingUser) return token;
+
+            token.role = existingUser.role;
 
             return token;
         },
         async session({ token, session }) {
             if (token.sub) {
                 session.user.id = token.sub;
+            }
+
+            if (token.role) {
+                session.user.role = token.role as UserRole;
             }
 
             return session;
