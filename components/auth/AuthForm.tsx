4c719914c2af977e0c1254c28848a450d46adf7c
WiseyXD <aryan.s.nag@gmail.com>
2024-04-09 15:24:47 +0530

    refactor : ui clean up

--- components/auth/AuthForm.tsx
+++ components/auth/AuthForm.tsx
@@ -80,7 +80,6 @@ export default function AuthForm({
                   });
         });
     }
-    // git fuck up
 
     return (
         // <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
