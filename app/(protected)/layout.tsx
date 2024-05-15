1de6a82f75d24345bbd6cb07c3c667e37607abd8
WiseyXD <aryan.s.nag@gmail.com>
2024-05-15 21:44:36 +0530

    null day

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -6,7 +6,7 @@ export interface IBaseTemplate {
 }
 
 export default async function BaseTemplate({ children }: IBaseTemplate) {
-    // Jai shree ram
+    // Jai shree
     const { session } = await validateRequest();
     if (!session) redirect('/login');
     return <div>{children}</div>;
