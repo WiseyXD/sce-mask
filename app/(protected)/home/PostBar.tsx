4ed5dcc974918fbe2892214544329a117bf3d300
WiseyXD <aryan.s.nag@gmail.com>
2024-07-01 04:28:58 +0530

    feat : file upload feature

--- app/(protected)/home/PostBar.tsx
+++ app/(protected)/home/PostBar.tsx
@@ -11,12 +11,13 @@ import { Input } from '@/components/ui/input';
 import { Separator } from '@/components/ui/separator';
 import { useToast } from '@/components/ui/use-toast';
 import { postCreationSchema } from '@/lib/schema';
+import { UploadDropzone } from '@/lib/uploadthing';
 import { imageLink } from '@/lib/utils';
 import { TUserDetails } from '@/types';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button as NextButton, User as NextUser } from '@nextui-org/react';
 import { Image, Smile } from 'lucide-react';
-import { useRef, useState, useTransition } from 'react';
+import { useRef, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
@@ -41,9 +42,10 @@ const icons = [
 ];
 
 export default function PostBar({ userDetails }: NonNullable<TUserDetails>) {
-    const [isPending, startTransisiton] = useTransition();
+    const [isPending, setIsPending] = useState<boolean>(false);
     const { toast } = useToast();
-    const [file, setFile] = useState<File | null>(null);
+    const [showDropzone, setShowDropzone] = useState<boolean>(false);
+    // const [file, setFile] = useState<File | null>(null);
     const [fileUrl, setFileUrl] = useState<string | undefined>(undefined);
     const fileInputRef = useRef<HTMLInputElement>(null);
 
@@ -54,21 +56,21 @@ export default function PostBar({ userDetails }: NonNullable<TUserDetails>) {
         },
     });
 
-    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const selectedFile = e.target.files?.[0] ?? null;
-        setFile(selectedFile);
+    // const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     const selectedFile = e.target.files?.[0] ?? null;
+    //     setFile(selectedFile);
 
-        if (fileUrl) {
-            URL.revokeObjectURL(fileUrl);
-        }
+    //     if (fileUrl) {
+    //         URL.revokeObjectURL(fileUrl);
+    //     }
 
-        if (selectedFile) {
-            const url = URL.createObjectURL(selectedFile);
-            setFileUrl(url);
-        } else {
-            setFileUrl(undefined);
-        }
-    };
+    //     if (selectedFile) {
+    //         const url = URL.createObjectURL(selectedFile);
+    //         setFileUrl(url);
+    //     } else {
+    //         setFileUrl(undefined);
+    //     }
+    // };
 
     async function onSubmit(values: z.infer<typeof postCreationSchema>) {
         // Do something with the form values.
@@ -78,32 +80,34 @@ export default function PostBar({ userDetails }: NonNullable<TUserDetails>) {
             return;
         }
         try {
-            startTransisiton(async () => {
-                const resp = await createPost({
-                    text: values.text,
-                    userId: userDetails.id,
-                    mediaLink: fileUrl ? fileUrl : '',
-                });
-                if (resp.success) {
-                    form.reset();
-                    setFile(null);
-                    setFileUrl(undefined);
-                    if (fileInputRef.current) {
-                        fileInputRef.current.value = ''; // Clear the file input
-                    }
-                    toast({
-                        title: 'Post created succesfully.',
-                    });
-                    console.log(resp.msg);
-                } else {
-                    console.log(resp.msg);
-                }
+            setIsPending(true);
+            const resp = await createPost({
+                text: values.text,
+                userId: userDetails.id,
+                mediaLink: fileUrl ? fileUrl : '',
             });
+            if (resp.success) {
+                form.reset();
+                // setFile(null);
+                setFileUrl(undefined);
+                if (fileInputRef.current) {
+                    fileInputRef.current.value = ''; // Clear the file input
+                }
+                toast({
+                    title: 'Post created succesfully.',
+                });
+                setIsPending(false);
+                console.log(resp.msg);
+            } else {
+                setIsPending(false);
+                console.log(resp.msg);
+            }
         } catch (error) {
             toast({
                 title: 'Error occured while post creation.',
                 variant: 'destructive',
             });
+            setIsPending(false);
         }
     }
 
@@ -144,7 +148,10 @@ export default function PostBar({ userDetails }: NonNullable<TUserDetails>) {
                             )}
                         />
                         <Separator />
-                        {fileUrl && file && (
+
+                        {/* File preview */}
+
+                        {/* {fileUrl && file && (
                             <div className="mt-4">
                                 {file.type.startsWith('image/') ? (
                                     <img src={fileUrl} alt="Selected file" />
@@ -152,27 +159,71 @@ export default function PostBar({ userDetails }: NonNullable<TUserDetails>) {
                                     <video src={fileUrl} controls />
                                 ) : null}
                             </div>
-                        )}
+                        )} */}
 
                         <div className="flex justify-between pt-3">
                             <div className="flex items-center gap-x-2 text-blue-600">
                                 {icons.map((item) => {
                                     return (
-                                        <div className="" key={item.text}>
-                                            {item.type == 'file' ? (
-                                                <Input
-                                                    ref={fileInputRef}
-                                                    type="file"
-                                                    accept="image/jpg,image/png,image/webp,image/jpg,image/gif,video/mp4,video/webm"
-                                                    name="media"
-                                                    onChange={handleFileChange}
+                                        <div
+                                            className="cursor-pointer"
+                                            key={item.text}
+                                        >
+                                            {showDropzone &&
+                                            item.type == 'file' ? (
+                                                <UploadDropzone
+                                                    className="cursor-pointer"
+                                                    endpoint="imageUploader"
+                                                    disabled={isPending}
+                                                    //         onBeforeUploadBegin={((files: File[]) => File[] | Promise<File[]> =>
+                                                    //             preview file throught url
+                                                    // )}
+                                                    onClientUploadComplete={(
+                                                        res
+                                                    ) => {
+                                                        // Do something with the response
+                                                        console.log(
+                                                            'Files: ',
+                                                            res[0]
+                                                        );
+                                                        setFileUrl(res[0].url);
+                                                        setIsPending(false);
+                                                        toast({
+                                                            title: 'File uploaded successfully.',
+                                                        });
+                                                    }}
+                                                    onUploadError={(
+                                                        error: Error
+                                                    ) => {
+                                                        // Do something with the error.
+                                                        setIsPending(false);
+                                                        toast({
+                                                            title: `ERROR! ${error.message}`,
+                                                            variant:
+                                                                'destructive',
+                                                        });
+                                                    }}
                                                 />
+                                            ) : item.type == 'file' ? (
+                                                <div
+                                                    className=""
+                                                    onClick={() =>
+                                                        setShowDropzone(true)
+                                                    }
+                                                >
+                                                    {item.icon}
+                                                </div>
                                             ) : (
                                                 item.icon
                                             )}
                                         </div>
                                     );
                                 })}
+                                {showDropzone ? (
+                                    <div onClick={() => setShowDropzone(false)}>
+                                        Cut
+                                    </div>
+                                ) : null}
                             </div>
                             <NextButton
                                 className="bg-blue-600 text-white  rounded-full"
