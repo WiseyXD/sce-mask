7e2ea87337ef01dd49636e349fb0711c3c17d63c
WiseyXD <aryan.s.nag@gmail.com>
2024-07-01 02:09:30 +0530

    fix : schema changes

--- lib/schema.ts
+++ lib/schema.ts
@@ -48,6 +48,25 @@ export const profileCreationSchema = z.object({
     }),
 });
 
+const MAX_FILE_SIZE = 5000000;
+const ACCEPTED_IMAGE_TYPES = [
+    'image/jpeg',
+    'image/jpg',
+    'image/png',
+    'image/webp',
+    'image/gif',
+    'video/mp4',
+    'video/webm',
+];
+
 export const postCreationSchema = z.object({
     text: z.string().min(1, { message: 'Text is required' }),
+
+    // media: z
+    //     .any()
+    //     .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
+    //     .refine(
+    //         (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+    //         'Only .jpg, .jpeg, .png and .webp formats are supported.'
+    //     ),
 });
