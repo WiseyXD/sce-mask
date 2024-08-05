703fd83db292640bc3f4b071f1797f9337f4f431
WiseyXD <aryan.s.nag@gmail.com>
2024-08-05 18:25:05 +0530

    build : schema changed for comment like

---
+++ prisma/migrations/20240805125214_cascade_condition_added/migration.sql
@@ -0,0 +1,35 @@
+-- DropForeignKey
+ALTER TABLE "Bookmark" DROP CONSTRAINT "Bookmark_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Bookmark" DROP CONSTRAINT "Bookmark_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "CommentLike" DROP CONSTRAINT "CommentLike_commentId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "CommentLike" DROP CONSTRAINT "CommentLike_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Like" DROP CONSTRAINT "Like_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Like" DROP CONSTRAINT "Like_userId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Bookmark" ADD CONSTRAINT "Bookmark_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Bookmark" ADD CONSTRAINT "Bookmark_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CommentLike" ADD CONSTRAINT "CommentLike_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CommentLike" ADD CONSTRAINT "CommentLike_commentId_fkey" FOREIGN KEY ("commentId") REFERENCES "Comment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
