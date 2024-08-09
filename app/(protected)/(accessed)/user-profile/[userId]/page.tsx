3ccb5c99e32bf55f63dedf9cc66e8923e21de2ab
WiseyXD <aryan.s.nag@gmail.com>
2024-08-09 11:42:19 +0530

    fix  : ui fix

--- app/(protected)/(accessed)/user-profile/[userId]/page.tsx
+++ app/(protected)/(accessed)/user-profile/[userId]/page.tsx
@@ -3,5 +3,9 @@ import { validateRequest } from '@/actions/validateRequests';
 export default async function page({ params }: { params: { userId: string } }) {
     const { session, user } = await validateRequest();
     const { userId } = params;
-    return <div>{userId}</div>;
+    return (
+        <div className="min-h-screen w-full">
+            <div>{userId}</div>
+        </div>
+    );
 }
