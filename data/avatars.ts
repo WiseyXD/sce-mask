794a340d1813adee7b46fd6da34ab31bd6b28eea
WiseyXD <aryan.s.nag@gmail.com>
2024-08-11 12:46:18 +0530

    fix : useless component removed

---
+++ data/avatars.ts
@@ -0,0 +1,7 @@
+export const avatarArray = [
+    { id: 1, link: 'https://ui.shadcn.com/avatars/01.png' },
+    { id: 2, link: 'https://ui.shadcn.com/avatars/02.png' },
+    { id: 3, link: 'https://ui.shadcn.com/avatars/03.png' },
+    { id: 4, link: 'https://ui.shadcn.com/avatars/04.png' },
+    { id: 5, link: 'https://ui.shadcn.com/avatars/05.png' },
+];
