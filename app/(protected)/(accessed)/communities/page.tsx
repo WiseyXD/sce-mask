04d235a4d01b78f1426c37a1a577f9251d57b710
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2024-09-21 23:53:55 +0530

    final waste day

--- app/(protected)/(accessed)/communities/page.tsx
+++ app/(protected)/(accessed)/communities/page.tsx
@@ -6,8 +6,6 @@ import CommunityBody from '@/components/CommunityBody';
 import CommunityHeader from '@/components/CommunityHeader';
 import { Separator } from '@/components/ui/separator';
 
-// comment added
-
 export default async function page() {
     const { user } = await validateRequest();
     const allDiscoverableCommunities =
